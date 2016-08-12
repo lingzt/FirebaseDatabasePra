0a50c367d746565eda981bbcfbdfc55ccef0db74
Ling <asklingtoby@gmail.com>
2016-08-11 23:31:50 -0400

    set up project

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirebaseDatabasePra
+//
+//  Created by ling toby on 8/10/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FirebaseDatabasePra
+//
+//  Created by ling toby on 8/10/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
